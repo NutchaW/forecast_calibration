699b7895c4b50027a4e925a6287e6fdf030bcd70
Nutcha Wattanachit <nutcha.wattanachit@gmail.com>
2022-02-24 14:35:38 -0500

    update code

--- plot_bcomp.R
+++
@@ -1,9 +0,0 @@
-a <- 6.875
-b <- 2066.612
-omega <- c(0.98,0.02)
-x<- comp_forecasts3 %*% omega
-cdf5<- pbeta(x,shape1=a,shape2=b)
-pdf5<- dbeta(x,shape1=a,shape2=b)
-
-plot(x,cdf5[,1])
-plot(x,pdf5[,1])
